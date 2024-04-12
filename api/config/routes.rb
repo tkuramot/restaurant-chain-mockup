cef1a8b9d7507b8df2fce7a0c040bc6b5e259a24
tkuramot <mabdman512@gmail.com>
2024-04-12 23:32:20 +0900

    Change routing

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :users, only: [:index]
+  scope '/api' do
+    resources :users, only: [:index]
+  end
 end
