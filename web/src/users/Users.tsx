d12f9a0f9c6001fd0c66d2d356592528aa2a2503
tkuramot <mabdman512@gmail.com>
2024-04-12 23:39:50 +0900

    configure environment variables

---
+++ web/src/users/Users.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+
+function Users() {
+  useEffect(() => {
+    // Get API server url from .env file
+  })
+
+  return (
+    <div>Users</div>
+  )
+}
+
+export default Users
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = env.VITE_API_URL;
+  console.log(API_URL)
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+          target: API_URL,
+          changeOrigin: true,
+        }
+			},
+		},
+	};
+});
